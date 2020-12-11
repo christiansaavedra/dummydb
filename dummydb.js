63b452792a2282f4d8fcf4f0a666330ffc4f9719
Chris <saavedra.christian.ok@gmail.com>
2020-12-11 12:33:14 -0300

    DB Certif

---
+++ dummydb.js
@@ -0,0 +1,44 @@
+certificados = [
+  {
+    titulo: "Certificado Coaching Ejecutivo",
+    activo: 1,
+    error: "",
+    datos: {
+      texto: "Ha completado sus estudios de Coaching Ejecutivo, etc...",
+      imagen:
+        "https://axont-certificacion.s3.us-east-2.amazonaws.com/posgrados/certificado_vacio_posgrados.png",
+      profesores: [{ nombre: "Oswaldo Vicente" }, { nombre: "Amalia Ledesma" }],
+      titulo: "Coaching Ejecutivo",
+      fechaClase: "26 de Diciembre de 2020",
+      nombreAlumno: "Christian Saavedra",
+    },
+  },
+  {
+    titulo: "Certificado Neuro Oratoria",
+    activo: 1,
+    error: "",
+    datos: {
+      texto: "Ha completado sus estudios de Neuro Oratoria, etc...",
+      imagen:
+        "https://axont-certificacion.s3.us-east-2.amazonaws.com/posgrados/certificado_vacio_posgrados.png",
+      profesores: [{ nombre: "Oswaldo Vicente" }, { nombre: "Amalia Ledesma" }],
+      titulo: "Neuro Oratoria",
+      fechaClase: "26 de Diciembre de 2020",
+      nombreAlumno: "Christian Saavedra",
+    },
+  },
+  {
+    titulo: "Certificado Coaching Jurídico",
+    activo: 0,
+    error: "En nuestros registros figura que aún no completaste el observado",
+    datos: {
+      texto: "Ha completado sus estudios de Neuro Oratoria, etc...",
+      imagen:
+        "https://axont-certificacion.s3.us-east-2.amazonaws.com/posgrados/certificado_vacio_posgrados.png",
+      profesores: [{ nombre: "Oswaldo Vicente" }, { nombre: "Amalia Ledesma" }],
+      titulo: "Neuro Oratoria",
+      fechaClase: "26 de Diciembre de 2020",
+      nombreAlumno: "Christian Saavedra",
+    },
+  },
+];
